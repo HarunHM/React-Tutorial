63568356d5b7e17da473cef7ca8e4a21af2b15c4
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-08-31 15:15:08 +0300

    debugged expression.jsx

---
+++ expressions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';  
+class App extends Component{  
+   render(){  
+      return(  
+         <div>  
+            <h1 className = "hello" >{25+20}</h1>  
+            {/* This is a comment in JSX */}
+            {/* output 45 */}
+         </div>  
+      );  
+   }  
+}  
+export default App;  
