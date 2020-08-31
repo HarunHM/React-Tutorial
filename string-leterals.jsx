4a3841697d7f8683fe21d4ba3aabb707a598be6c
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-08-31 15:18:24 +0300

    created string-literals.jsx

---
+++ string-leterals.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';  
+class App extends Component{  
+   render(){  
+      return(  
+         <div>  
+            <h1 className = "hello" >#100 Days of JavaScript</h1>  
+            <p data-demoAttribute = "demo">
+              The best Frontend course in 2020.
+            </p>  
+         </div>  
+      );  
+   }  
+}  
+export default App;  
