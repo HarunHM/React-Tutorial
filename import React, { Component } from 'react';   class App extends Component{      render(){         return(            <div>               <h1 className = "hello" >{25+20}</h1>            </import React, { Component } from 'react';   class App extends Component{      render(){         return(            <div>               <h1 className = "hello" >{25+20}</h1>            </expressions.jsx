ba3accd579f63e8a4c450897291f1a430b3b10a7
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-08-31 15:10:44 +0300

    Created expression.jsx file

---
+++ import React, { Component } from 'react';   class App extends Component{      render(){         return(            <div>               <h1 className = "hello" >{25+20}</h1>            </import React, { Component } from 'react';   class App extends Component{      render(){         return(            <div>               <h1 className = "hello" >{25+20}</h1>            </expressions.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';  
+class App extends Component{  
+   render(){  
+      return(  
+         <div>  
+            <h1 className = "hello" >{25+20}</h1>  
+         </div>  
+      );  
+   }  
+}  
+export default App;
